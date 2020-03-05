ddc4700669b646c31b435a485acae73b6b7e6524
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-03-05 11:29:59 +0200

    Integrate frontend to the maven project.
    The frontend is now integrated to work with the mavne project.
    The frontend is also added to the jar file.

--- src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
+++ src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
@@ -2,12 +2,24 @@ package fi.tuni.shs.Blog.Platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BlogPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogPlatformApplication.class, args);
+	}
+
+	@RestController
+	public class HelloController {
+		@GetMapping("/api/hello")
+		public String hello() {
+			return "Hello, the time at the server is now " + new Date() + "\n";
+		}
 	}
 
 }
