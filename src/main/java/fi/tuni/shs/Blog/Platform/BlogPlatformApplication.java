a8f60714a8cc59ebd455fe3b3ca966fdeb8e72ef
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-03-05 11:06:09 +0200

    Initial commit.

---
+++ src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.shs.Blog.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.shs.Blog.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
