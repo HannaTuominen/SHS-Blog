f0f53cafc4ccdc9bd789a3ed10d8239c9e7bb40e
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-10 15:38:41 +0200

    Add admin pw login info to system.out.print.

--- src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
+++ src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
@@ -17,7 +17,7 @@ public class BlogPlatformApplication {
 	public static void main(String[] args) {
 		System.out.println("Maija Visala, Hanna Tuominen, Viljami Pietarila");
 		System.out.println("Commands that can be used for REST testing.");
-		System.out.println("Necessary user name / passwords needed for testing");
+		System.out.println("Necessary user name: admin / passwords: admin needed for testing");
 		System.out.println(System.getenv("JDBC_DATABASE_URL"));
 		SpringApplication.run(BlogPlatformApplication.class, args);
 		if (System.getenv("JDBC_DATABASE_URL") != null) {
