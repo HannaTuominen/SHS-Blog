87c15cda3c2cd64fc33557a29cd747058c4b3939
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2020-03-12 13:47:53 +0200

    Change the http auth with authorities for testing.

--- src/main/java/fi/tuni/shs/Blog/Platform/SecurityConfiguration.java
+++ src/main/java/fi/tuni/shs/Blog/Platform/SecurityConfiguration.java
@@ -17,16 +17,55 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").authorities("ADMIN_AUTH");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/api/hello")
+                .hasAuthority("ADMIN_AUTH")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/api/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
                 .and()
                 .httpBasic();
+
+//                http
+//                .authorizeRequests()
+//                .antMatchers( "/api/helloo").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+
     }
     @Bean
     PasswordEncoder passwordEncoder() {
