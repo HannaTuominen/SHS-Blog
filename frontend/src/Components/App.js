d576221a8461711b73b4c0f8bf2ac8db97889a81
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-03-27 15:26:40 +0200

    Move postData and postsData to APp' 's state

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -5,30 +5,50 @@ import BlogPost from "./Blogposts/BlogPost";
 
 
 export default class extends Component {
-  postsData = [];
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPost: {title: '', body: ''},
+      postsData: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPosts()
+    this.fetchPost(1)
+  }
 
   fetchPosts = () => {
     fetch('/api/getAllPosts/',{
       mode: 'no-cors', // 'cors' by default
       credentials: "same-origin"
-    }).then(data => data.json()).then(this.updatePostsData).catch(this.serverError)
+    }).then(data => data.json())
+      .then(data => this.setState({postsData: data}))
+      .catch(this.serverError)
+  }
+
+  fetchPost = (id) => {
+    fetch("/api/get/" + id)
+      .then(data => data.json())
+      .then(data => this.setState({currentPost : data}))
+      .catch(this.serverError)
   }
 
   serverError = (data) => {
-    console.log('Server error while fetching posts data')
     console.log(data)
   }
 
-  updatePostsData = (data) => {
-    this.postsData = data;
+  changeId = (id) => {
+    this.fetchPost(id);
   }
 
   render() {
+    console.log(this.state.postsData)
+    console.log(this.state.currentPost)
     return <Box bgcolor= "secondary.light"  position="absolute">
       <Header/>
       <BlogPost height="100%"/>
       <Footer />
     </Box>
-
   }
 }
