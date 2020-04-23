d7a7d6993c37f82c16727aa99a3b43c2341efa29
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-04-23 18:23:49 +0300

    Heroku hosting test.

--- src/main/java/fi/tuni/shs/Blog/Platform/BlogPost.java
+++ src/main/java/fi/tuni/shs/Blog/Platform/BlogPost.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class BlogPost {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private long id;
 
     private String body;
     private String title;
