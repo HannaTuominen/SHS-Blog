af07c9627b63274f44a3e42e96c7d7d39695e068
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-05-06 18:37:03 +0300

    Add missing curl commands.

--- src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
+++ src/main/java/fi/tuni/shs/Blog/Platform/BlogPlatformApplication.java
@@ -5,13 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 
 @SpringBootApplication
 @EnableConfigurationProperties(FileStorageProperties.class)
@@ -27,17 +20,32 @@ public class BlogPlatformApplication implements CommandLineRunner {
 		System.out.println("Curl commands");
 		System.out.println("-------------");
 		System.out.println("GET all posts.");
-		System.out.println("curl -X GET http://localhost:8080/api/get/");
+		System.out.println("	curl -X GET http://localhost:8080/api/get/");
 		System.out.println("GET one post.");
-		System.out.println("curl -X GET http://localhost:8080/api/get/1");
+		System.out.println("	curl -X GET http://localhost:8080/api/get/1");
 		System.out.println("GET comments of one post.");
-		System.out.println("curl -X GET http://localhost:8080/api/getComments/1");
+		System.out.println("	curl -X GET http://localhost:8080/api/getComments/1");
 		System.out.println("GET log data.");
-		System.out.println("curl -X GET http://localhost:8080/api/getLogs/");
+		System.out.println("	curl -X GET http://localhost:8080/api/getLogs/");
 		System.out.println("GET posts title and id data.");
-		System.out.println("curl -X GET http://localhost:8080/api/getAllPosts/");
+		System.out.println("	curl -X GET http://localhost:8080/api/getAllPosts/");
 		System.out.println("GET time and greeting from server.");
-		System.out.println("curl -X GET http://localhost:8080/api/hello");
+		System.out.println("	curl -X GET http://localhost:8080/api/hello");
+		System.out.println("POST a post.");
+		System.out.println("	curl -X POST --user admin:admin -H \"Content-type: application/json\" " +
+				"-d '{\"body\":\"post body\", \"title\":\"post title\"}' 'http://localhost:8080/api/add/'");
+		System.out.println("POST a comment.");
+		System.out.println("	curl -XPOST --user admin:admin -H \"Content-type: application/json\" -d " +
+				"'{\"parentPost\": 1, \"name\":\"commentor name\", \"message\": \"comment message\", " +
+				"\"time\": 1000, \"thumbsUp\": \"5\"}' 'http://localhost:8080/api/addComment/'");
+		System.out.println("POST thumbs up to one comment.");
+		System.out.println("	curl -X POST --user admin:admin http://localhost:8080/api/thumbsUp/1");
+		System.out.println("POST thumbs down to one comment.");
+		System.out.println("	curl -X POST --user admin:admin http://localhost:8080/api/thumbsDown/1");
+		System.out.println("DELETE one post.");
+		System.out.println("	curl -X DELETE --user admin:admin http://localhost:8080/api/delete/1");
+		System.out.println("DELETE one comment.");
+		System.out.println("	curl -X DELETE --user admin:admin http://localhost:8080/api/deleteComment/1");
 		System.out.println("Necessary user name: admin / passwords: admin needed for testing");
 	}
 }
